bbfe39dd85e9af1bda5a5a9d1d410665ae8a3f79
Deadvitekchpool <32135426+Deadvitekchpool@users.noreply.github.com>
2021-12-21 23:07:55 +0300

    Task 11 fixes

--- Assignment11.cpp
+++ Assignment11.cpp
@@ -8,11 +8,13 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size); //set the size of mpi communicator(the number of processes)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); //set the ranks for processes
     int message = 1; //message to send
-    int rec_message; //message to receive
     //MPI_Sendrecv function simply sends message to the next process and receiving rec_message from previous process
-    MPI_Sendrecv(&message, 1, MPI_INT, rank == size - 1 ? 0 : rank + 1, 0, 
-    &rec_message, 1, MPI_INT, rank == 0 ? size - 1 : rank - 1, 0, MPI_COMM_WORLD, &status);
-    printf("Process %d received message %d from process %d\n", rank, rec_message_one, status.MPI_SOURCE);
+    MPI_Sendrecv_replace(&message, 1, MPI_INT, rank == size - 1 ? 0 : rank + 1, 0,
+     rank == 0 ? size - 1 : rank - 1, 0, MPI_COMM_WORLD, &status);
+    printf("Process %d received message %d from process %d\n", rank, message, status.MPI_SOURCE);
+    MPI_Sendrecv_replace(&message, 1, MPI_INT, rank == 0 ? size - 1 : rank - 1, 0,
+     rank == size - 1 ? 0 : rank + 1, 0, MPI_COMM_WORLD, &status);
+    printf("Process %d received message %d from process %d\n", rank, message, status.MPI_SOURCE);
 
     MPI_Finalize(); //shut down mpi services
     return 0;
